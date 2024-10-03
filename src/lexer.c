aae84024034c65b5a37183a4240be148c2d350cd
Jacob <jacob.signorovitch@gmail.com>
2024-10-02 21:04:54 -0400

    Added .clang-format.

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@ Lexer* lexer_init(char* src) {
     lexer->src = src;
     lexer->srcl = strlen(src);
     lexer->cchar = lexer->src;
-    
+
     lexer->tokens = NULL;
     lexer->ntokens = 0;
     lexer->state = LEXER_STATE_CONFUSED;
@@ -14,31 +14,21 @@ Lexer* lexer_init(char* src) {
     return lexer;
 }
 
-void lexer_destroy(Lexer *lexer) {
+void lexer_destroy(Lexer* lexer) {
     free(lexer->src);
 
-    for (
-        int i = 0;
-        i < lexer->ntokens;
-        token_destroy(lexer->tokens[i++])
-    );
+    for (int i = 0; i < lexer->ntokens; token_destroy(lexer->tokens[i++]));
 }
 
-void lexer_do_confused(Lexer *lexer) {
-
-}
+void lexer_do_confused(Lexer* lexer) {}
 
 void lexer_lex(Lexer* lexer) {
     while (*lexer->cchar) {
         switch (lexer->state) {
-        case LEXER_STATE_CONFUSED:
-            lexer_do_confused(lexer);
-        break; case LEXER_STATE_NUM:
-            lexer_do_number(lexer);
-        break; case LEXER_STATE_CALL:
-            lexer_do_call(lexer);
-        break;
-        default: break;
+        case LEXER_STATE_CONFUSED: lexer_do_confused(lexer); break;
+        case LEXER_STATE_NUM:      lexer_do_number(lexer); break;
+        case LEXER_STATE_CALL:     lexer_do_call(lexer); break;
+        default:                   break;
         }
     }
 }
